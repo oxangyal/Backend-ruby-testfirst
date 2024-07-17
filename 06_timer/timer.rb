e8f364ac1435ce7b499d48e41b3a6b00e604d99d
Oxana Michkasova <oxana.mitch@gmail.com>
2024-07-16 22:40:04 -0400

    Timer methods

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    minutes = (@seconds % 3600) / 60
+    seconds = @seconds % 60
+
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(number)
+    number.to_s.rjust(2, '0')
+  end
 end
