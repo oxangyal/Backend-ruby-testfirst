0372b27f064e99c3317e363d070b33001441f224
Oxana Michkasova <oxana.mitch@gmail.com>
2024-07-16 22:39:30 -0400

    Book class

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+
+    # write your code here
+
+    attr_reader :title
+
+    def title=(new_title)
+        little_words = %w[a an the and in of over]
+        words = new_title.split.map.with_index do |word, index|
+          if index == 0 || !little_words.include?(word)
+            word.capitalize
+          else
+            word
+          end
+        end
+        
+        @title = words.join(' ')
+      end
+
 end
